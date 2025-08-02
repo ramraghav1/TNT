749965ed0efa84151d2cfd8e0e0e0682a508e15d
ramraghav1 <ramshranlamichhane@gmail.com>
2025-08-02 16:06:56 +0545

    git ignore

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a0f0d4dbe8678e06f921ed502c40f7c9be362d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1690b414d8d4c9d59d40391d1783f370b993ee5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a0f0d4dbe8678e06f921ed502c40f7c9be362d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1690b414d8d4c9d59d40391d1783f370b993ee5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
